7ff04ab595a1cf5ff38663fc6f9b438d0b44ca08
yvan-frank <yvanzangue@gmail.com>
2024-07-16 12:28:02 +0100

    fix conflict

--- src/app/page.js
+++ src/app/page.js
@@ -2,22 +2,17 @@ import Image from "next/image";
 import DefaultLayout from "@/layout/DefaulLayout";
 import Component2 from "@/components/Home/Component2";
 import Component1 from "@/components/Home/Component1";
-<<<<<<< HEAD
 import Component4 from "@/components/Home/Component4";
-=======
 import Component3 from "@/components/Home/Component3";
->>>>>>> a29f69c973365557aea1f991de720117aa6457f1
+
 
 export default function Home() {
   return (
     <DefaultLayout>
         <Component2 />
         <Component1 />
-<<<<<<< HEAD
-        <Component4 />
-=======
         <Component3 />
->>>>>>> a29f69c973365557aea1f991de720117aa6457f1
+        <Component4 />
     </DefaultLayout>
   );
 }
