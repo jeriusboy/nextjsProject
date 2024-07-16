c7bb3b140f50969bc00f48abcfc5106e22b66962
jeriusboy <jerius38@gmail.com>
2024-07-16 14:45:27 +0100

    component 5

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Component2 from "@/components/Home/Component2";
 import Component1 from "@/components/Home/Component1";
 import Component4 from "@/components/Home/Component4";
 import Component3 from "@/components/Home/Component3";
+import Component5 from "@/components/Home/Component5";
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <Component1 />
         <Component3 />
         <Component4 />
+        <Component5 />
     </DefaultLayout>
   );
 }
--- src/data.js
+++ src/data.js
@@ -2,6 +2,10 @@ import cheaper from "./assets/cheaper.png";
 import faster from "./assets/faster.png";
 import reach from "./assets/reach.png";
 import support from "./assets/support.png";
+import BusinessCard1 from "./assets/business-model-icon-1.png";
+import BusinessCard2 from "./assets/business-model-icon-2.png";
+import BusinessCard3 from "./assets/business-model-icon-3.png";
+
 
 export const dataCard1 = [
 	{
@@ -45,5 +49,26 @@ export const dataCard4 = [
 		id: 3,
 		title: "Unified Commerce ›",
 		description: "Connect your online and in-store payment data in one system.",
+	},
+]
+
+export const dataCard5 = [
+	{
+		id: 1,
+		title: "Open an Account ›",
+		description: "Get an account number and virtual card instantly upon approval. Manage it all from your..",
+		img: BusinessCard1,
+	},
+	{
+		id: 2,
+		title: "No hidden fees ›",
+		description: "Enjoy the lowest transfer fees in the market with transparent pricing and no monthly fees.",
+		img: BusinessCard2,
+	},
+	{
+		id: 3,
+		title: "All in one place ›",
+		description: "No need for separate accounts, cards, or software. Scale faster with a fully integrated..",
+		img: BusinessCard3,
 	},
 ]
