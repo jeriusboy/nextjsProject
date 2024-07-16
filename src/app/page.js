04b9442a11505b79f53641f83d982bcb65d35ddd
yvan-frank <yvanzangue@gmail.com>
2024-07-16 14:50:20 +0100

    fix unmerged files

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,8 @@ import Component2 from "@/components/Home/Component2";
 import Component1 from "@/components/Home/Component1";
 import Component4 from "@/components/Home/Component4";
 import Component3 from "@/components/Home/Component3";
-<<<<<<< HEAD
-import Component6 from "@/components/Home/Component6";
-=======
 import Component5 from "@/components/Home/Component5";
->>>>>>> c7bb3b140f50969bc00f48abcfc5106e22b66962
-
+import Component6 from "@/components/Home/Component6";
 
 export default function Home() {
   return (
@@ -18,12 +14,8 @@ export default function Home() {
         <Component1 />
         <Component3 />
         <Component4 />
-<<<<<<< HEAD
-
-        <Component6 />
-=======
         <Component5 />
->>>>>>> c7bb3b140f50969bc00f48abcfc5106e22b66962
+        <Component6 />
     </DefaultLayout>
   );
 }
