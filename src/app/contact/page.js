8f5d198cba67249cdfe5f4756a8a017ce79cbfcb
yvan-frank <yvanzangue@gmail.com>
2024-07-22 12:24:09 +0100

    blog

---
+++ src/app/contact/page.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import DefaultLayout from "@/layout/DefaulLayout";
+import Image from "next/image";
+import integration from "@/assets/integrations-banner-bg.jpg";
+
+const Page = () => {
+	return (
+		<DefaultLayout>
+			<div className="w-full bg-cyan-950 p-5 relative">
+				<Image src={integration} alt="Blog"
+				       className="absolute inset-0 h-full w-full object-cover bg-opacity-80"/>
+				<div className="relative z-10 flex flex-col items-center justify-center h-full">
+					<div className="flex flex-col items-center justify-center my-10 lg:my-20 gap-5">
+						<h1 className="text-4xl lg:text-7xl font-bold text-white">Contact</h1>
+						<p className="text-base lg:text-xl text-gray-300">Get in touch</p>
+					</div>
+				</div>
+			</div>
+		</DefaultLayout>
+	);
+};
+
+export default Page;
--- src/components/Home/Component2.js
+++ src/components/Home/Component2.js
@@ -14,30 +14,43 @@ function Component2() {
           Making Global Payments Simple
         </h1>
         <div className=" lg:pr-20 pt-4">
-          <p className="text-cyan-700 lg:text-2xl  text-lg leading-8 font-light  ">
+          <p className="text-cyan-800 lg:text-2xl  text-lg leading-8 font-light  ">
             Start growing your business with our innovative payment solutions.
           </p>
         </div>
-        <div className="grid grid-cols-1 lg:flex lg:items-center grid-rows-4 md:flex md:items-center  md:grid-cols-2 pt-8">
-          <div>
-            <button className="bg-amber-500 leading-[30px] xs:text-sm hover:bg-transparent hover:ease-in hover:text-amber-500 mr-4 font-semibold text-slate-100 px-5 py-1 rounded border border-transparent hover:border hover:border-cyan-950">
-              Open a free account
-            </button>
-          </div>
-          <div>
-            <button className="hover:bg-amber-500 leading-[30px] bg-transparent hover:ease-in text-amber-500 font-semibold hover:text-slate-100 px-5 py-1 rounded border border-cyan-950 hover:border-transparent hover:border">
-             
-              see How it works
-            </button>
-          </div>
+        <div className="flex gap-5 lg:gap-10 mt-10 flex-wrap">
+          <button
+              className="px-5 py-2 md:py-3 bg-cyan-600 text-white rounded-lg font-semibold tracking-wider hover:bg-[#F2F3F9]
+              hover:text-cyan-950 border hover:border-cyan-950/50 duration-200 border-transparent"
+          >
+            Open a Free Account
+          </button>
+          <button
+              className="px-5 py-2 md:py-3 border border-cyan-950/50 rounded-lg font-semibold text-cyan-800 tracking-wider
+              hover:bg-cyan-600 hover:text-white duration-200"
+          >
+            See How it Works
+          </button>
+
+          {/*<div>*/}
+          {/*  <button className="bg-amber-500 leading-[30px] xs:text-sm hover:bg-transparent hover:ease-in hover:text-amber-500 mr-4 font-semibold text-slate-100 px-5 py-1 rounded border border-transparent hover:border hover:border-cyan-950">*/}
+          {/*    Open a free account*/}
+          {/*  </button>*/}
+          {/*</div>*/}
+          {/*<div>*/}
+          {/*  <button className="hover:bg-amber-500 leading-[30px] bg-transparent hover:ease-in text-amber-500 font-semibold hover:text-slate-100 px-5 py-1 rounded border border-cyan-950 hover:border-transparent hover:border">*/}
+          {/*   */}
+          {/*    see How it works*/}
+          {/*  </button>*/}
+          {/*</div>*/}
         </div>
       </div>
       <div className="w-2/4 pt-10 hidden lg:block md:hidden">
         <Image
-          src={BannerImage}
-          width={1000}
-          height={1000}
-          alt="banner imgage"
+            src={BannerImage}
+            width={1000}
+            height={1000}
+            alt="banner imgage"
         />
       </div>
     </div>
